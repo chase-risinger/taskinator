2906fb68f4167194bd8918fd663cef3079840de4
chase-risinger <ccr1988@gmail.com>
2021-10-03 13:03:32 -0500

    Adding more functionality to the form and submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,18 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.dir(taskNameInput);
 
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+    var taskInfoE1 = document.createElement("div");
+    taskInfoE1.className = "task-info";
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemE1.appendChild(taskInfoE1);
     tasksToDoE1.appendChild(listItemE1);
-    console.log(event);
+
 }
 
 formE1.addEventListener("submit", createTaskHandler);
@@ -31,22 +37,3 @@ formE1.addEventListener("submit", createTaskHandler);
 
 
 
-
-var counter = 10;
-var countdown = function () {
-    console.log(counter);
-    counter--;
-    if (counter === 0) {
-        console.log("blastoff");
-        clearInterval(startCountdown);
-    };
-};
-
-var startCountdown = setInterval(countdown, 1000);
-
-var sayHello = function () {
-    console.log("hello there");
-
-};
-
-var timedGreeting = setTimeout(sayHello, 2000);

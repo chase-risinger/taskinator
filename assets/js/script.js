6c43d31194fb83aeb17178c3ef5104369c5cd922
chase-risinger <ccr1988@gmail.com>
2021-10-03 12:08:22 -0500

    Gave functionality to the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,36 @@
-var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1);
+var formE1 = document.querySelector("#task-form");
 
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
+var createTaskHandler = function (event) {
+    event.preventDefault();
 
-var createTaskHandler = function () {
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
     tasksToDoE1.appendChild(listItemE1);
+    console.log(event);
 }
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var counter = 10;
 var countdown = function () {
